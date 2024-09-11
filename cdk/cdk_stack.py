4a157ba46485fdf4e00dc9aa115e7247de95b733
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-09-11 11:50:39 -0400

    feat: Initial Commit

---
+++ cdk/cdk_stack.py
@@ -0,0 +1,48 @@
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
+    Duration,
+    aws_dynamodb as dynamodb,
+)
+from constructs import Construct
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class CdkStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        table = dynamodb.TableV2(self, f"Table-{construct_id}",
+            table_name=f"{construct_id}",
+            partition_key=dynamodb.Attribute(name="SessionId", type=dynamodb.AttributeType.STRING),
+            # sort_key=dynamodb.Attribute(name="timestamp", type=dynamodb.AttributeType.STRING)
+        )
+        
+        dockerFunc = _lambda.DockerImageFunction(
+            scope=self,
+            id=f"Lambda-{construct_id}",
+            function_name=construct_id,
+            environment= {
+                "DISCORD_PUBLIC_KEY" : os.getenv('DISCORD_PUBLIC_KEY'),
+                "DISCORD_ID" : os.getenv('DISCORD_ID'),
+                "PINECONE_INDEX_NAME" : os.getenv('PINECONE_INDEX_NAME'),
+                "PINECONE_API_KEY" : os.getenv('PINECONE_API_KEY'),
+                "OPENAI_API_KEY" : os.getenv('OPENAI_API_KEY'),
+            },            
+            code=_lambda.DockerImageCode.from_image_asset(
+                directory="src"
+            ),
+            timeout=Duration.seconds(300),
+            memory_size=256,
+        )
+
+        api = apigateway.LambdaRestApi(self, f"API-{construct_id}",
+            handler=dockerFunc,
+            proxy=True,
+        )
+
+        table.grant_read_write_data(dockerFunc)
---
+++ src/app/main.py
@@ -0,0 +1,106 @@
+import os
+import requests
+import json
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
+def handler(event, context):
+    '''
+    Entry point for Lambda function
+    '''
+
+    # Verify the cryptographic Signature
+    if not verify(event):
+        return {'statusCode': 401, 'body': json.dumps('Unauthorized')}
+
+    body = json.loads(event['body'])
+
+    if body['type'] == 1:
+        return {'statusCode': 200, 'body': json.dumps({'type': 1})}
+    else:
+        return interact(body)
+
+
+def interact(raw_request):
+    '''
+    Handle the interaction request
+    '''
+
+    # Discord sends a request based on the slash command entered
+    # This will intepret the request
+    # Store Interactions TOKEN and ID
+    os.environ["INTERACTIONS_TOKEN"] = raw_request["token"]
+    os.environ["INTERACTIONS_ID"] = raw_request["id"]
+
+    # Immediately send an interaction response back to discord to prevent a timeout
+    send()
+
+    data = raw_request["data"]
+    userID = raw_request["member"]["user"]["id"]
+
+    print(f"The Interaction Was Executed By User: {userID}")
+    print(f"Full Interactions Structure {data}")
+
+    # The command being executed
+    command_name = data["name"]
+
+    match command_name:
+        # Command /chat [arg1: message]
+        case "chat":
+            message = data["options"][0]["value"]
+            import llm
+            result = llm.invoke_llm(message, userID)
+            print(f"LLM Response: {result}")
+
+            # Count number of characters
+            if len(result) > 2000:
+                result = result[:2000] + "..."
+
+            update(result)
+
+        # Command /dog
+        # Sends a link embedded within the link's image of a dog   
+        case "dog":
+            message_content = requests.get("https://dog.ceo/api/breeds/image/random").json().get("message")
+            update(message_content)
+
+    return {}
+
+def verify(event):
+    '''
+    Validates the request signature. This makes sure that the Interactions API request is coming from Discord
+    '''
+    signature = event['headers']['x-signature-ed25519']
+    timestamp = event['headers']['x-signature-timestamp']
+    verify_key = VerifyKey(bytes.fromhex(os.environ.get("DISCORD_PUBLIC_KEY")))
+    message = timestamp + event['body']
+
+    try:
+        verify_key.verify(message.encode(), signature=bytes.fromhex(signature))
+    except BadSignatureError:
+        print("Bad signature")
+        return False
+    return True
+
+# Utility Functions For Discord API
+def send():
+    url = f"https://discord.com/api/interactions/{os.getenv('INTERACTIONS_ID')}/{os.getenv('INTERACTIONS_TOKEN')}/callback"
+    response = requests.post(url, json={"type": 5})
+    
+    print("Response status code: ")
+    print(response.status_code)
+    print(response.text)
+
+def update(message):
+    url = f"https://discord.com/api/webhooks/{os.getenv('DISCORD_ID')}/{os.getenv('INTERACTIONS_TOKEN')}"
+
+    data = {
+        "content": message
+    }
+
+    # Send the PATCH request
+    response = requests.post(url, json=data)
+
+    print("Response status code: ")
+    print(response.status_code)
+    print(response.text)
